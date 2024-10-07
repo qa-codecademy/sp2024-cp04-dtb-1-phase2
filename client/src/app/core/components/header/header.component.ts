4b19f8fa0f9d1e51000e298932c80b300b580fde
Ana-Jovanovska <150249028+Ana-Jovanovska@users.noreply.github.com>
2024-10-07 14:53:35 +0200

    add hamburger manu

--- client/src/app/core/components/header/header.component.ts
+++ client/src/app/core/components/header/header.component.ts
@@ -2,11 +2,12 @@ import { Component, effect, inject, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { ThemeService } from '../../services/theme.service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, NgClass],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
