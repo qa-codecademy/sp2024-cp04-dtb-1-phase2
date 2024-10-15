27213860bcab465340b313169418206fdd775cb5
BojanJankov <150026975+BojanJankov@users.noreply.github.com>
2024-10-15 18:42:53 +0200

    added add user details option in user panel

--- client/src/app/feature/auth/components/add-user-details/add-user-details.component.ts
+++ client/src/app/feature/auth/components/add-user-details/add-user-details.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { UserDetailsFormComponent } from '../user-details-form/user-details-form.component';
+import { AuthService } from '../../../../core/services/auth.service';
+import { Router } from '@angular/router';
+import { CreateUserDetailsReq } from '../../models/auth.model';
 
 @Component({
   selector: 'app-add-user-details',
@@ -8,4 +11,14 @@ import { UserDetailsFormComponent } from '../user-details-form/user-details-form
   templateUrl: './add-user-details.component.html',
   styleUrl: './add-user-details.component.scss',
 })
-export class AddUserDetailsComponent {}
+export class AddUserDetailsComponent {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  currentUser = this.authService.currentUser;
+
+  onAddUserDetails(addUserDetails: CreateUserDetailsReq) {
+    this.authService.createUserDetails(this.currentUser().id, addUserDetails);
+    this.router.navigate(['user-details']);
+  }
+}
